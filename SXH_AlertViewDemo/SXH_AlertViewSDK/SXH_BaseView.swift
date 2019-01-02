9869da1a515a3ffeea75409f0be321763ed683f8
sun <814997453@qq.com>
2019-01-02 12:04:00 +0800

    测试做的修改

--- SXH_AlertViewDemo/SXH_AlertViewSDK/SXH_BaseView.swift
+++ SXH_AlertViewDemo/SXH_AlertViewSDK/SXH_BaseView.swift
@@ -98,7 +98,6 @@ class SXH_BaseView: UIView {
         UIView.animate(withDuration: 0.34, animations: { [unowned self] in
             self.costomView.alpha = 1.0
         })
-        debugPrint("ddd")
         
     }
     
